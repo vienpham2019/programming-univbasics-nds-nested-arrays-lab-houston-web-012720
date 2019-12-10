09e005358d442c8a99927751654b8481b7397dc8
vien pham <vienpham2019@gmail.com>
2019-12-10 04:26:35 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -19,7 +19,7 @@ ENFIELD_TENNIS_ACADEMY = [
 def assembled_matrix
   # Build an array that contains (or, "nests") the residents of The Ennet House
   # and the Enfield Tennis Academy as provided by the constants
-  my_new_array = [ENNET_HOUSE, ENFIELD_TENNIS_ACADEMY]
+  my_new_array = [ENNET_HOUSE.sort, ENFIELD_TENNIS_ACADEMY.sort]
   return my_new_array
 end
 
